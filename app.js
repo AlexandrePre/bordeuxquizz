f05ebe53513c6efcc28ea62bc1a48b59aa1c2a47
JULIEN DUSSART <julien.dussart59@gmail.com>
2022-09-28 14:16:20 +0200

    scroll and landing page

---
+++ app.js
@@ -0,0 +1,27 @@
+
+const header = document.getElementsByTagName("header");
+
+const container = document.querySelector('.container');
+
+const footer = document.querySelector("h3");
+
+const quizz1 = document.querySelector('.quizz1');
+
+
+header.style.display = 'none';
+footer.style.display = 'none';
+
+window.addEventListener('scroll', () => {
+    const { scrollTop, clientHeight } = document.documentElement;
+    const topContainerToTopViewport = quizz1.getBoundingClientRect().top;
+    if (scrollTop > topContainerToTopViewport.toFixed()) {
+        header.style.display = 'block';
+        footer.style.display = 'block';
+    }
+});
+
+
+
+
+
+

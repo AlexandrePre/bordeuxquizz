3735a2b1426607e3de524f779eceee8edd439bb4
JULIEN DUSSART <julien.dussart59@gmail.com>
2022-09-28 16:50:53 +0200

    modif

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const header = document.querySelector(".header");
 
 const container = document.querySelector('.container');
 
-const footer = document.querySelector("h3");
+const footer = document.querySelector(".footer");
 
 const quizz1 = document.querySelector('.quizz1');
 
 
 header.style.display = 'none';
-footer.style.display = 'none';
+footer.style.display = 'block';
 
 window.addEventListener('scroll', () => {
     const { scrollTop, clientHeight } = document.documentElement;
@@ -18,7 +18,7 @@ window.addEventListener('scroll', () => {
         header.style.display = 'block';
         footer.style.display = 'block';
     } else {header.style.display = 'none';
-    footer.style.display = 'none';
+            footer.style.display = 'none';
 }
 });
 

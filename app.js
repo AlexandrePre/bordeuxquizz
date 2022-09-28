dcbdf5c98c97a41f5f72bff5b45793fcee6c3f01
AlexandrePre <pre.alexandre1@gmail.com>
2022-09-28 16:35:46 +0200

    last features

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-<<<<<<< HEAD
-const header = document.querySelector(".header");
-=======
 
 const header = document.querySelector(".header");
 
->>>>>>> 0e4b43fc57c9101184aadf3fd2f0940b622dca4f
 const container = document.querySelector('.container');
-const footer = document.querySelector("h3");
+const footer = document.querySelector('.footer')
 const quizz1 = document.querySelector('.quizz1');
+const team = document.querySelector('containerTeam1');
 
 
 header.style.display = 'none';
 footer.style.display = 'none';
+
 
 window.addEventListener('scroll', () => {
 
@@ -22,22 +20,10 @@ window.addEventListener('scroll', () => {
     if (scrollTop > topContainerToTopViewport.toFixed()) {
         header.style.display = 'block';
         footer.style.display = 'block';
-<<<<<<< HEAD
     } else {
         header.style.display = 'none';
         footer.style.display = 'none';
 
     }
-
-=======
-    } else {header.style.display = 'none';
-    footer.style.display = 'none';
-}
->>>>>>> 0e4b43fc57c9101184aadf3fd2f0940b622dca4f
 });
-
-
-
-
-
 

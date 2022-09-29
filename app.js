68cdffebe3131cd040931e2ee8fc72def71ca42c
NicolasBaudet <baudetnicolas@hotmail.fr>
2022-09-29 20:57:54 +0200

    update before pull

--- app.js
+++ app.js
@@ -17,13 +17,60 @@ window.addEventListener('scroll', () => {
     if (scrollTop > topContainerToTopViewport.toFixed()) {
         header.style.display = 'block';
         footer.style.display = 'block';
-    } else {header.style.display = 'none';
-    footer.style.display = 'none';
-}
+    } else {
+        header.style.display = 'none';
+        footer.style.display = 'none';
+    }
 });
 
+// slide horizontal
 
+const items = document.querySelectorAll('.question');
+const nbSlide = items.length;
+const suivant = document.querySelector('.right');
+const precedent = document.querySelector('.left');
+let count = 0;
 
+items[count].style.display = 'block';
+items[count + 1].style.display = 'none';
+items[count + 2].style.display = 'none';
+
+function slideSuivante() {
+    items[count].style.display = 'none';
+
+    if (count < nbSlide - 1) {
+        count++;
+    } else {
+        count = 0;
+    }
+
+    items[count].style.display = 'block';
+}
+suivant.addEventListener('click', slideSuivante);
+
+function slidePrecedente() {
+    items[count].style.display = 'none';
+
+    if (count > 0) {
+        count--;
+    } else {
+        count = nbSlide - 1;
+    }
+
+    items[count].style.display = 'block';
+}
+precedent.addEventListener('click', slidePrecedente);
+
+function keyPress(e) {
+    console.log(e);
+
+    if (e.keyCode === 37) {
+        slidePrecedente();
+    } else if (e.keyCode === 39) {
+        slideSuivante();
+    }
+}
+document.addEventListener('keydown', keyPress)
 
 
 

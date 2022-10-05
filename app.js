4920ede18ef9266f5ea748c3f6a209a84d0e157d
JULIEN DUSSART <julien.dussart59@gmail.com>
2022-10-05 08:57:53 +0200

    update 2

--- app.js
+++ app.js
@@ -1,89 +1,70 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable func-names */
 // scroll header
-const header = document.querySelector(".header");
-const container = document.querySelector(".container");
-let height = container.clientHeight;
-
-window.addEventListener("scroll", () => {
-    if (window.scrollY > height - 5) {
-        header.classList.add("scroll");
-    } else {
-        header.classList.remove("scroll");
-    }
+const header = document.querySelector('.header');
+const container = document.querySelector('.container');
+const height = container.clientHeight;
+window.addEventListener('scroll', () => {
+  if (window.scrollY > height - 5) {
+    header.classList.add('scroll');
+  } else {
+    header.classList.remove('scroll');
+  }
 });
 
-//scroll footer
-const team = document.querySelector(".team");
-const footer = document.querySelector(".footer");
-let height1 = team.clientHeight;
-
-window.addEventListener("scroll", () => {
-    if (window.scrollY > height1 - 2) {
-        footer.classList.add("scroll1");
-    } else {
-        footer.classList.remove("scroll1");
-    }
+// scroll footer
+const team = document.querySelector('.team');
+const footer = document.querySelector('.footer');
+const height1 = team.clientHeight;
+window.addEventListener('scroll', () => {
+  if (window.scrollY > height1 + 2222) {
+    footer.classList.add('scroll1');
+  } else {
+    footer.classList.remove('scroll1');
+  }
 });
-
-
 
 // formulaire pour rentrer le nom des équipes
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
-const team1 = document.getElementById("team1");
-const team2 = document.getElementById("team2");
-const text1 = document.getElementById("text1");
-const text2 = document.getElementById("text2");
-
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const team1 = document.getElementById('team1');
+const team2 = document.getElementById('team2');
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
 btn1.addEventListener('click', () => {
-    team1.innerHTML = text1.value;
-    text1.value = "";  
+  team1.innerHTML = text1.value;
+  text1.value = '';
 });
-
 btn2.addEventListener('click', () => {
-    team2.innerHTML = text2.value;
-    text2.value = "";
+  team2.innerHTML = text2.value;
+  text2.value = '';
 });
 
 // open modal
-const btnWin = document.getElementById("btnWin");
-btnWin.addEventListener("click",  () => {
-const random = Math.floor(Math.random() * 100) + 1;
-const modal = document.getElementById("modal");
-const teamWin = document.getElementById("teamWin");
+const btnWin = document.getElementById('btnWin');
+btnWin.addEventListener('click', () => {
+  const random = Math.floor(Math.random() * 100) + 1;
+  const modal = document.getElementById('modal');
+  const teamWin = document.getElementById('teamWin');
+  if (random < 50) {
+    ((modal.style.display = 'flex') && (teamWin.innerHTML = team1.innerHTML));
+  } else { ((modal.style.display = 'flex') && (teamWin.innerHTML = team2.innerHTML)); }
 
- 
-
-    if (random < 50) { 
-        ((modal.style.display = "flex") && (teamWin.innerHTML = team1.innerHTML));
-        } else { ((modal.style.display = "flex") && (teamWin.innerHTML = team2.innerHTML)) };
-    
-    btnWin.onsubmit = function (event) {
-        event.preventDefault();
-    };
-
-    });
-
-
-const form = document.getElementById("form");
+  btnWin.onsubmit = function (event) {
+    event.preventDefault();
+  };
+});
+const form = document.getElementById('form');
 form.onsubmit = function (event) {
   event.preventDefault();
 };
 
-// empèche de reload
-
-
-
-
-// close modal    
-const btnClose = document.getElementById("btnClose");
-const modal = document.getElementById("modal");
-btnClose.addEventListener("click", () => {
-    modal.style.display = "none";
-   
+// close modal
+const btnClose = document.getElementById('btnClose');
+const modal = document.getElementById('modal');
+btnClose.addEventListener('click', () => {
+  modal.style.display = 'none';
 });
-
 btnClose.onsubmit = function (event) {
   event.preventDefault();
 };
-
-

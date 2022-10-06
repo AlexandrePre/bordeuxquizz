e6bcfb115db20b7860398f68cbac3d5aa586ab7f
JULIEN DUSSART <julien.dussart59@gmail.com>
2022-10-06 10:11:05 +0200

    update

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+/* eslint-disable brace-style */
+/* eslint-disable no-undef */
+/* eslint-disable no-loop-func */
+/* eslint-disable no-inner-declarations */
+/* eslint-disable no-plusplus */
+/* eslint-disable eqeqeq */
+/* eslint-disable quotes */
+/* eslint-disable no-shadow */
+/* eslint-disable padded-blocks */
+/* eslint-disable indent */
 /* eslint-disable no-unused-expressions */
 /* eslint-disable func-names */
 // scroll header
@@ -13,11 +23,13 @@ window.addEventListener('scroll', () => {
 });
 
 // scroll footer
-const team = document.querySelector('.team');
+const quizz1 = document.querySelector('.quizz1');
+const team = document.querySelector(".team")
 const footer = document.querySelector('.footer');
-const height1 = team.clientHeight;
+const height1 = quizz1.clientHeight;
+const height2 = team.clientHeight;
 window.addEventListener('scroll', () => {
-  if (window.scrollY > height1 + 2222) {
+  if (window.scrollY > height1 + height2 - 10) {
     footer.classList.add('scroll1');
   } else {
     footer.classList.remove('scroll1');
@@ -68,3 +80,114 @@ btnClose.addEventListener('click', () => {
 btnClose.onsubmit = function (event) {
   event.preventDefault();
 };
+
+const items = document.querySelectorAll('.question');
+const nbSlide = items.length;
+const suivant = document.querySelector('.right');
+const precedent = document.querySelector('.left');
+let slideIndex = 0;
+const answer2 = document.querySelectorAll('h3');
+const wrongInGreen = document.querySelectorAll('.rightAnswer');
+
+items[slideIndex].style.display = 'block';
+items[slideIndex + 1].style.display = 'none';
+items[slideIndex + 2].style.display = 'none';
+items[slideIndex + 3].style.display = 'none';
+items[slideIndex + 4].style.display = 'none';
+
+function slideSuivante() {
+    items[slideIndex].style.display = 'none';
+
+    if (slideIndex < nbSlide - 1) {
+        slideIndex++;
+    } else {
+        slideIndex = 1;
+    }
+
+    items[slideIndex].style.display = 'block';
+
+    // remise à zéro des classes bonne et mauvaise réponse : function dans la function
+
+    function testAnswer2() {
+
+        for (let i = 0; i < answer2.length; i++) {
+
+            const testAnswer2 = answer2[i].className;
+
+            if (testAnswer2 == 'wrongAnswerRed') {
+                answer2[i].className = "wrongAnswer";
+            }
+            if (testAnswer2 == 'rightAnswerGreen') {
+                answer2[i].className = "rightAnswer";
+            }
+        }
+    }
+
+    testAnswer2();
+
+}
+suivant.addEventListener('click', slideSuivante);
+
+function slidePrecedente() {
+    items[slideIndex].style.display = 'none';
+
+    if (slideIndex > 1) {
+        slideIndex--;
+    } else {
+        slideIndex = nbSlide - 1;
+    }
+
+    items[slideIndex].style.display = 'block';
+
+    // remise à zéro des classes bonne et mauvaise réponse : function dans la function
+
+    function testAnswer2() {
+
+        for (let i = 0; i < answer2.length; i++) {
+
+            const testAnswer2 = answer2[i].className;
+
+            if (testAnswer2 == 'wrongAnswerRed') {
+                answer2[i].className = "wrongAnswer";
+            }
+            if (testAnswer2 == 'rightAnswerGreen') {
+                answer2[i].className = "rightAnswer";
+            }
+        }
+    }
+
+    testAnswer2();
+}
+
+function keyPress(e) {
+    if (e.keyCode === 37) {
+        slidePrecedente();
+    } else if (e.keyCode === 39) {
+        slideSuivante();
+    }
+}
+document.addEventListener('keydown', keyPress);
+
+// function background color change
+
+const scoreCanneleTexte = document.querySelector('.scorecannele');
+const scoreMacaronTexte = document.querySelector('.scoremacaron');
+let scoreCannele = 0;
+let scoreMacaron = 0;
+for (let i = 0; i < answer2.length; i++) {
+    function testAnswer() {
+        const testAnswer = answer2[i].className;
+        if (testAnswer == 'wrongAnswer') {
+            answer2[i].className = "wrongAnswerRed";
+            wrongInGreen[slideIndex - 1].className = "rightAnswerGreen";
+        }
+        if (testAnswer == 'rightAnswer') {
+            answer2[i].className = "rightAnswerGreen";
+            if (slideIndex % 2 == 0) {
+                scoreCannele++; scoreCanneleTexte.innerHTML = scoreCannele;
+            // eslint-disable-next-line semi
+            } else scoreMacaron++;
+            scoreMacaronTexte.innerHTML = scoreMacaron;
+        }
+    }
+    answer2[i].addEventListener('click', testAnswer); }
